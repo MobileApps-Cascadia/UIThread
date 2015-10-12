2b43fd364aa154aff53ba30808b0a2fca5e883f3
Cascadia-PawanjitNayyar <pawanjit.kaur1@gmail.com>
2015-10-12 15:04:34 -0700

    UIThread Exercise
    Completed:
    2. Update the code using the Task class from this INCORRECT Threads
    tutorial (Links to an external site.) (ignore the for loop - it is
    replaced by the takeSomeTime(int) method) . Note which of the Android
    Threading Rules is violated.
    3. Modify the Android project using this Thread Handler tutorial (Links
    to an external site.) which uses a handler to work with thread
    correctly. (instead of creating a handler, just use the bar View as the
    handler.)

--- app/src/main/java/edu/cascadia/brianb/uithread/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/uithread/MainActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.os.Handler;
+
+//import java.util.logging.Handler;
 
 public class MainActivity extends Activity {
     private ProgressBar bar;
+    private Handler handler;
 
     /** Called when the activity is first created. */
 
@@ -14,29 +18,52 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
         bar = (ProgressBar) findViewById(R.id.progressBar1);
-        bar.setProgress(0);
+       // bar.setProgress(0); 3rd Exercise
 
     }
 
     public void startProgress(View view) {
-        bar.setProgress(0);
-        for (int i = 0; i < 10; i++) {
+       // bar.setProgress(0); // Commented for 3rd Exercise
+        new Thread(new Task()).start();
+        // Commented for 2nd Exercise
+        /*for (int i = 0; i < 10; i++) {
             final int count = i+1;
             takeSomeTime(5); //cause the current thread to delay for given seconds
             bar.setProgress(count);
-        }
+        }*/
     }
 
-    public void takeSomeTime(int seconds){
-        long endTime = System.currentTimeMillis() + seconds*1000;
+    class Task implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                final int count = i+1;
+                takeSomeTime(5); //cause the current thread to delay for given seconds
+                //bar.setProgress(count);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        bar.setProgress(count);
+                    }
 
-        while (System.currentTimeMillis() < endTime) {
-            synchronized (this) {
-                try {
-                    wait(endTime - System.currentTimeMillis());
-                } catch (Exception e) {}
-            }
+                } );
         }
+    }
+    public void takeSomeTime(int seconds){
+        // Commented for 2nd Exercise
+        //long endTime = System.currentTimeMillis() + seconds*1000;
+
+        //while (System.currentTimeMillis() < endTime) {
+            //synchronized (this) {
+                try {
+                    //wait(endTime - System.currentTimeMillis());  // Commented for 2nd Exercise
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                e.printStackTrace();}
+            }
+           // }
+        //}
     }
 }
