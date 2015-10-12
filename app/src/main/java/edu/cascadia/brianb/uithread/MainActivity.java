bcd9ac6057688dccc8be58c10e46f9a371e142f4
Hiromi <hiromif630@gmail.com>
2015-10-12 15:06:34 -0700

    Finished two thread exercises
    Finished two thread exercises

--- app/src/main/java/edu/cascadia/brianb/uithread/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/uithread/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
     private ProgressBar bar;
+    private Handler handler;
 
     /** Called when the activity is first created. */
 
@@ -15,16 +17,44 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bar = (ProgressBar) findViewById(R.id.progressBar1);
-        bar.setProgress(0);
+        handler = new Handler();
+        // bar.setProgress(0); old
 
     }
 
     public void startProgress(View view) {
-        bar.setProgress(0);
-        for (int i = 0; i < 10; i++) {
+        //bar.setProgress(0); old
+        new Thread (new Task()).start();
+        takeSomeTime(0);
+
+        // Original code from GitHub
+       /* for (int i = 0; i < 10; i++) {
             final int count = i+1;
+
             takeSomeTime(5); //cause the current thread to delay for given seconds
             bar.setProgress(count);
+        }*/
+    }
+
+    class Task implements Runnable{
+        @Override
+        public void run(){
+            for (int i = 0; i < 10; i++){
+                final int count = i+1;
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                //bar.setProgress(count); old
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        bar.setProgress(count);
+                    }
+                });
+            }
         }
     }
 
